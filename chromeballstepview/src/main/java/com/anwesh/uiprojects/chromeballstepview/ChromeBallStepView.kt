75639ab43f97a7716fa48d403be3e32a766716ae
Anwesh43 <anweshthecool0@gmail.com>
2018-11-19 10:01:12 +0530

    adding CBSNode class

--- chromeballstepview/src/main/java/com/anwesh/uiprojects/chromeballstepview/ChromeBallStepView.kt
+++ chromeballstepview/src/main/java/com/anwesh/uiprojects/chromeballstepview/ChromeBallStepView.kt
@@ -116,8 +116,53 @@ class ChromeBallStepView(ctx : Context) : View(ctx) {
 
         fun stop() {
             if (animated) {
-                animated = false 
+                animated = false
             }
+        }
+    }
+
+    data class CBSNode(var i : Int, val state : State = State()) {
+
+        private var next : CBSNode? = null
+
+        private var prev : CBSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = CBSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCBSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CBSNode {
+            var curr : CBSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
         }
     }
 }
