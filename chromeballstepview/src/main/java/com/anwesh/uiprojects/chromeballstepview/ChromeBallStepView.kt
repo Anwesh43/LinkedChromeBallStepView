079902a3822cf7ea67f8286805d7079666cc8046
Anwesh43 <anweshthecool0@gmail.com>
2018-11-19 09:33:51 +0530

    adding Float and Int extension methods

--- chromeballstepview/src/main/java/com/anwesh/uiprojects/chromeballstepview/ChromeBallStepView.kt
+++ chromeballstepview/src/main/java/com/anwesh/uiprojects/chromeballstepview/ChromeBallStepView.kt
@@ -18,5 +18,15 @@ val ball_color : Int = Color.parseColor("#1A237E")
 val arc_colors : Array<String> = arrayOf("#f44336", "#FDD835", "#388E3C")
 val SIZE_FACTOR : Int = 3
 val STROKE_FACTOR : Int = 60
-val scDiv : Float = 0.51f
+val scDiv : Double = 0.51
 val scGap : Float = 0.05f
+
+fun Int.getInverse() : Float = 1f / this
+
+fun Float.divideScale(i : Int, n : Int) : Float = Math.max(n.getInverse(), this - i * n.getInverse())
+
+fun Float.getScaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+
+fun Float.getMirrorValue(a : Int, b : Int) : Float = (1 - this) * a.getInverse() + this * b.getInverse()
+
+fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = this.getMirrorValue(a, b) * dir * scGap
