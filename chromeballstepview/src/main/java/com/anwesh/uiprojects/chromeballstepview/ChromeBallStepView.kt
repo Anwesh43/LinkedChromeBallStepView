894c357ef4c9942991c795284691e5031ff73573
Anwesh43 <anweshthecool0@gmail.com>
2018-11-19 10:03:34 +0530

    adding ChromeBallStep class

--- chromeballstepview/src/main/java/com/anwesh/uiprojects/chromeballstepview/ChromeBallStepView.kt
+++ chromeballstepview/src/main/java/com/anwesh/uiprojects/chromeballstepview/ChromeBallStepView.kt
@@ -165,4 +165,30 @@ class ChromeBallStepView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ChromeBallStep(var i : Int) {
+
+        private val root : CBSNode = CBSNode(0)
+
+        private var curr : CBSNode = root
+
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
