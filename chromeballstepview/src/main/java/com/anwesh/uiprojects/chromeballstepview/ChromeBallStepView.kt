622ab45a193f6f2ce76058095ff4c554e0828718
Anwesh43 <anweshthecool0@gmail.com>
2018-11-19 09:23:14 +0530

    adding libraries and defining constants

--- chromeballstepview/src/main/java/com/anwesh/uiprojects/chromeballstepview/ChromeBallStepView.kt
+++ chromeballstepview/src/main/java/com/anwesh/uiprojects/chromeballstepview/ChromeBallStepView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.chromeballstepview
 /**
  * Created by anweshmishra on 19/11/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val ball_color : Int = Color.parseColor("#1A237E")
+val arc_colors : Array<String> = arrayOf("#f44336", "#FDD835", "#388E3C")
+val SIZE_FACTOR : Int = 3
+val STROKE_FACTOR : Int = 60
+val scDiv : Float = 0.51f
+val scGap : Float = 0.05f
