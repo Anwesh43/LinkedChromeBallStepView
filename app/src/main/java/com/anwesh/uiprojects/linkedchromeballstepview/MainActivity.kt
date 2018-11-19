db6cab9ff32a270be9060898d829381d5c9d9261
Anwesh43 <anweshthecool0@gmail.com>
2018-11-19 10:11:04 +0530

    adding chromeballstepview in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedchromeballstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedchromeballstepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedchromeballstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.chromeballstepview.ChromeBallStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ChromeBallStepView.create(this)
     }
 }
